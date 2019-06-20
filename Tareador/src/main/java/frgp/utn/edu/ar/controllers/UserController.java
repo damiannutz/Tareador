458b2a208138b2f15e2e5d6b685582521990b7c6
rubioden22 <rubioden22@hotmail.com>
2019-06-20 12:50:29 -0300

    Inicio nuevo

---
+++ Tareador/src/main/java/frgp/utn/edu/ar/controllers/UserController.java
@@ -0,0 +1,119 @@
+package frgp.utn.edu.ar.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Convert;
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.exception.spi.ConversionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import frgp.utn.edu.ar.dominio.Usuario;
+import frgp.utn.edu.ar.servicio.UsuarioServicio;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	public  UsuarioServicio service;
+	
+	public void init(ServletConfig config) {
+		ApplicationContext ctx = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(config.getServletContext());
+		
+		this.service = (UsuarioServicio) ctx.getBean("serviceBean");
+	}
+	
+	
+	//Inicio
+	
+	@RequestMapping("IrLogin.html")
+	public ModelAndView redireccion(){
+		ModelAndView MV = new ModelAndView();
+		MV.addObject("listaUsuarios",this.service.obtenerUsuarios());
+		MV.setViewName("Index"); 
+		return MV;
+	}
+	
+	
+	@RequestMapping(value ="/altaUsuario.html" , method= { RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView validarUsuario(String nombreU, String passU){
+		ModelAndView MV = new ModelAndView();
+		
+		Usuario x = new Usuario();
+		x.setNombre(nombreU);
+		x.setContrasenia(passU);
+		
+		String Message="";
+		
+		try{
+			
+			service.insertarUsuario(x);
+			Message = "Usuario agregado";
+		}
+		catch(Exception e)
+		{
+			Message = "No se pudo insertar el usuario";
+		}
+		finally
+		{
+		
+		}
+	
+		MV.setViewName("Usuarios");
+		MV.addObject("Mensaje", Message);
+		MV.addObject("listaUsuarios",this.service.obtenerUsuarios());
+		MV.setViewName("Usuarios"); 
+		return MV;
+		
+	}
+	
+     
+	@RequestMapping(value ="/eliminarUsuario.html" , method= { RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView eliminarUsuario(Integer id, String nombreU, String passU){
+		ModelAndView MV = new ModelAndView();
+		service.eliminarUsuario(id);
+		MV.addObject("listaUsuarios",this.service.obtenerUsuarios());
+		MV.setViewName("Usuarios"); 
+		MV.addObject("Mensaje", "Usuario eliminado");
+		return MV;
+	}
+	
+	@RequestMapping(value = { "/delete-user-{ssoId}" }, method = RequestMethod.GET)
+    public ModelAndView deleteUser(@PathVariable Integer ssoId) {
+		service.eliminarUsuario(ssoId);
+		ModelAndView MV = new ModelAndView();
+		MV.setViewName("Usuarios");
+		
+		//Actualiza los usuarios
+		MV.addObject("listaUsuarios",this.service.obtenerUsuarios());
+		MV.setViewName("Usuarios"); 
+		return MV;
+    }
+	
+
+ 
+	
+	@RequestMapping(value ="/recargaGrillaUsuarios.html" , method= { RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView recargarUsuario(){
+		ModelAndView MV = new ModelAndView();
+		MV.addObject("listaUsuarios",this.service.obtenerUsuarios());
+		MV.setViewName("Usuarios"); 
+		return MV;
+		
+	}
+	
+}
---
+++ Tareador/src/main/java/frgp/utn/edu/ar/dominio/ComentarioTarea.java
@@ -0,0 +1,76 @@
+package frgp.utn.edu.ar.dominio;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+
+
+@Entity
+
+@Table(name = "Comentarios_Tareas")
+public class ComentarioTarea {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="Id_Comentario_Tarea")
+	private Integer IdComentarioTarea;
+	
+	@ManyToOne(cascade= {CascadeType.ALL})
+	@JoinColumn(name="Id_Tarea")
+	private Tarea tarea;
+	
+	@Column(name="Fecha_Registro")
+	private Date FechaRegistro;
+
+	@Column(name="Comentario")
+	private String Comentario;
+	
+	public Integer getIdComentarioTarea() {
+		return IdComentarioTarea;
+	}
+
+	public void setIdComentarioTarea(Integer idComentarioTarea) {
+		IdComentarioTarea = idComentarioTarea;
+	}
+
+	public Tarea getTarea() {
+		return tarea;
+	}
+
+	public void setTarea(Tarea tarea) {
+		this.tarea = tarea;
+	}
+
+	public Date getFechaRegistro() {
+		return FechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		FechaRegistro = fechaRegistro;
+	}
+
+	public String getComentario() {
+		return Comentario;
+	}
+
+	public void setComentario(String comentario) {
+		Comentario = comentario;
+	}
+
+	public ComentarioTarea(Integer idComentarioTarea, Tarea tarea, Date fechaRegistro, String comentario) {
+		super();
+		IdComentarioTarea = idComentarioTarea;
+		this.tarea = tarea;
+		FechaRegistro = fechaRegistro;
+		Comentario = comentario;
+	}
+
+	public ComentarioTarea() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
